60855b62eec475f6896fc1cf3d6e6e0b03273936
Osama-Atwa <ashrafosama536@gmail.com>
2022-08-09 15:08:08 +0200

    config some Bugs
    solve the negative amounts issue and make the website more a live.

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -10,8 +10,8 @@ export class HomeComponent implements OnInit {
   img: string = '../../../assets/img1.jpg';
   count: number = 0;
   products = [
-    {product: new ProductModule(0,'S10','../../../assets/s10.jpg',10,10000,'mobile') , count: 0, sum: 0},
-    {product: new ProductModule(1,'S20','../../../assets/s20.jpg',20,20000,'mobile') , count: 0, sum: 0},
+    {product: new ProductModule(0,'S10','../../../assets/s10.jpg',1,10000,'mobile') , count: 0, sum: 0},
+    {product: new ProductModule(1,'S20','../../../assets/s20.jpg',0,20000,'mobile') , count: 0, sum: 0},
     {product: new ProductModule(2,'Dell','../../../assets/Dell.jpg',5,10000,'laptop') , count: 0, sum: 0},
     {product: new ProductModule(3,'Lenovo','../../../assets/lenovo.jpg',6,20000,'laptop') , count: 0, sum: 0}
   ];
@@ -36,15 +36,20 @@ export class HomeComponent implements OnInit {
   }
 
   OnBuy(id:number){
-    if(this.products.find(x=>x.product.id==id))
+    var x = this.products.filter(x=>x.product.id==id);
+    if(x[0] && x[0].product.qty>0 && x[0].product.qty - x[0].count >= 0)
     {
-      var x = this.products.filter(x=>x.product.id==id);      ;
       x[0].sum = x[0].count * x[0].product.Price;
+      if(x[0].product.qty > 0)
+      {
+        x[0].product.qty = x[0].product.qty - x[0].count;
+      }
       this.calTotal();
     }
+
   }
   calTotal(){
-    this.total=0;
+    // this.total=0;
     for (let index = 0; index < this.products.length; index++) {
       const element = this.products[index];
       this.total += element.sum;
